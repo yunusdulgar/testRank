f72c315c661701b6d1035a2c2decee3acf07fb8f
yunusdulgar <dulgaryunus@gmail.com>
2016-07-02 14:50:49 +0300

    first

---
+++ HackerRank/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int n = in.nextInt();
+		
+		int prisonCount = in.nextInt();
+		int sweetsCount = in.nextInt();
+		int startId = in.nextInt();
+		
+		int[][] prisons = new int[prisonCount][1];
+
+		for (int i = 0; i < prisons.length; i++) {
+			prisons[i][0] = i+1 ;
+		}
+		
+		int left = prisonCount%sweetsCount;
+
+		 for (int i = 0; i < prisons.length; i++) {
+			System.out.println(left);
+		}
+		
+	}
+
+}
